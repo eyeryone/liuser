9fdb34f208b8f69a9a448f508a690f0086e905d3
liupinyang <80465@qq.com>
2019-01-02 20:08:32 +0800

    第一次修改

--- src/Test.java
+++ src/Test.java
@@ -4,6 +4,10 @@ public class Test {
     public static void main(String[] args) {
 
         System.out.println("hello");
-
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
     }
 }
